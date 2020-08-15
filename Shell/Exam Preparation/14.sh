b32d4602354493385b9d2bdeb35f48bfe3124e5c
Stanislav <st1nka17@abv.bg>
2020-08-15 14:40:56 +0300

    Update 14.sh

--- Shell/Exam Preparation/14.sh
+++ Shell/Exam Preparation/14.sh
@@ -18,7 +18,7 @@ while read user
 do
 	to_kill=$(mktemp)
 	ps -e -o rss,pid,user | grep  $user | sort -rn | awk -v max=$1 'NR==1 {ppid=$2; cnt=0} {cnt=cnt+$1} END{if(cnt>max) print ppid}' >to_kill
-	kill $(cat to_kill)
+	kill $(cat to_kill) 2>/dev/null
 
 
 done < USERS
