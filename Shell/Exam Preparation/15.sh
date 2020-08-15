e4c4fc7e88287f52092b745bc91c27def1794bd6
Stanislav <st1nka17@abv.bg>
2020-08-15 14:53:36 +0300

    Update 15.sh

--- Shell/Exam Preparation/15.sh
+++ Shell/Exam Preparation/15.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while read USER HOMEDIR; do
-	if [ $(sudo -u $user touch -c $HOMEDIR 2>&1 | grep -q 'Peirmission denied') ] || [ ! -d $HOMEDIR ]; then
+	if [ $(sudo -u $user touch -c $HOMEDIR 2>&1 | grep -q 'Permission denied') ] || [ ! -d $HOMEDIR ]; then
 		cat /etc/passwd | awk -v user=$user' FS=":" {if($1==user) print $0}'
 	fi
 done < <(cat /etc/passwd | cut -d':' -f16)
